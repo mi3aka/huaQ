fcc635929a1b1c7b6fdc0f821c7f54e0c6e02c18
mi3aka <mi3aka@outlook.com>
2022-08-04 00:08:24 +0800

    update

--- 基础环境/php74/www/check.php
+++ 基础环境/php74/www/check.php
@@ -3,7 +3,7 @@ highlight_file(__FILE__);
 $world = "World";
 echo "Hello, ", $world . "<br>";
 
-new mysqli("172.17.0.1", "root", "root", "mysql");
+new mysqli("172.17.0.1", "root", "root", "mysql","4000");
 if (mysqli_connect_errno()) { #检查连接
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
@@ -11,6 +11,6 @@ if (mysqli_connect_errno()) { #检查连接
 echo "连接成功";
 if (!is_dir('test_mkdir')) {
     mkdir('test_mkdir');
+    rmdir('test_mkdir');
 }
-phpinfo();
-?>
+phpinfo(3);
